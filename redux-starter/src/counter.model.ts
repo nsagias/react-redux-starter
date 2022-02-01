b6cc514034a06568f32349aea1ca03a7753ac864
Nick <nsagias@gmail.com>
2022-01-31 23:40:19 -0500

    refactor interfaces and rename Action to CounterAction

--- redux-starter/src/counter.model.ts
+++ redux-starter/src/counter.model.ts
@@ -2,7 +2,22 @@ export interface IState {
   counter: number;
 }
 
+
+export interface IIncrement {
+  type: "increment";
+  INCREMENT: string;
+}
+
+export interface IDecrement {
+  type: 'decrement';
+  DECREMENT: string;
+}
+
+
+export type CounterAction = IIncrement | IDecrement;
+
+
 export interface ICounterReducer {
   state: {counter: number, type: {}};
-  action: {};
+  action: CounterAction;
 }
--- redux-starter/src/store/index.ts
+++ redux-starter/src/store/index.ts
@@ -1,20 +1,7 @@
 import { useEffect, useState } from "react";
 import { createStore } from "redux";
-import { IState, ICounterReducer } from "../counter.model";
+import { IState, ICounterReducer, CounterAction } from "../counter.model";
 import { INITIAL_STATE_COUNTER, INCREMENT, DECREMENT } from "../constants";
-
-
-interface IIncrement {
-  type: "increment";
-  INCREMENT: string;
-}
-
-interface IDecrement {
-  type: 'decrement';
-  DECREMENT: string;
-}
-
-type Action = IIncrement | IDecrement;
 
 
 const [initialState, setIinitialState] = useState<IState>(INITIAL_STATE_COUNTER);
@@ -23,7 +10,7 @@ useEffect(() => {
   setIinitialState(initialState);
 }, []);
 
-const counterReducer = (state:IState = { counter: 0 }, action: Action) => {
+const counterReducer = (state:IState = { counter: 0 }, action: CounterAction) => {
   if (action.type === INCREMENT) {
     return {
       counter: state.counter + 1
