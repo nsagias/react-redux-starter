944cf1c91d2f75819eb877b23f626143c4690619
Nick <nsagias@gmail.com>
2022-02-01 00:34:42 -0500

    comment out useState and useEffect

--- redux-starter/src/store/index.ts
+++ redux-starter/src/store/index.ts
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { createStore } from "redux";
 import { IState, ICounterReducer, CounterAction } from "../counter.model";
 import { INITIAL_STATE_COUNTER, INCREMENT, DECREMENT } from "../constants";
 
 
-const [initialState, setIinitialState] = useState<IState>(INITIAL_STATE_COUNTER);
-useEffect(() => {
-  // call from api 
-  setIinitialState(initialState);
-}, []);
+// const [initialState, setIinitialState] = useState<IState>(INITIAL_STATE_COUNTER);
+// useEffect(() => {
+//   // call from api 
+//   setIinitialState(initialState);
+// }, []);
 
 const counterReducer: ICounterReducer = (state:IState = INITIAL_STATE_COUNTER, action: CounterAction) => {
   if (action.type === INCREMENT) {
@@ -26,3 +26,5 @@ const counterReducer: ICounterReducer = (state:IState = INITIAL_STATE_COUNTER, a
 };
 
 const store = createStore(counterReducer);
+
+export default store;
