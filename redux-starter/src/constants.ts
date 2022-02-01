c70cf1292af044cc8bc129e93166148348da421f
Nick <nsagias@gmail.com>
2022-02-01 16:39:10 -0500

    Add increase to constants

--- redux-starter/src/constants.ts
+++ redux-starter/src/constants.ts
@@ -3,4 +3,5 @@ import { IState } from "./counter.model";
 export const INITIAL_STATE_COUNTER: IState = { counter: 0 };
 
 export const INCREMENT: string = "INCREMENT";
+export const INCREASE: string = "INCREASE";
 export const DECREMENT: string = "DECREMENT";
