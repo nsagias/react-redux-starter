874660685a48b6316b4fac72b878be11820e0c70
Nick <nsagias@gmail.com>
2022-01-31 22:36:33 -0500

    create interface for counter reducer

---
+++ redux-starter/src/store/index.ts
@@ -0,0 +1,10 @@
+import {createStore} from "redux";
+
+interface ICounterReducer {
+  state: {};
+  action: {}
+}
+
+const counterReducer: ICounterReducer = (state:{}, action:{}) => {};
+
+const store = createStore();
