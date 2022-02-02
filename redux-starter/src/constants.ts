ce231c1419cf84f5085d9ed2109241a583c80bf8
Nick <nsagias@gmail.com>
2022-02-02 17:36:23 -0500

    import model and create initial state authentication

--- redux-starter/src/constants.ts
+++ redux-starter/src/constants.ts
@@ -1,6 +1,8 @@
 import { IState } from "./counter.model";
+import { IAuthentication } from "./authentication.model";
 
 export const INITIAL_STATE_COUNTER: IState = { counter: 0, showCounter: true};
+export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false}
 
 export const COUNTER: string = "counter";
 export const INCREMENT: string = "INCREMENT";
