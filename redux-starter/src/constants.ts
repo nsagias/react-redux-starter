fcae7dd95fc3be43424f21dd503149a991938a00
Nick <nsagias@gmail.com>
2022-02-02 18:48:37 -0500

    import showCounter model and refactor

--- redux-starter/src/constants.ts
+++ redux-starter/src/constants.ts
@@ -1,8 +1,11 @@
 import { IState } from "./counter.model";
 import { IAuthentication } from "./authentication.model";
+import { IShowCounter } from "./showCounter.model";
 
-export const INITIAL_STATE_COUNTER: IState = { counter: 0, showCounter: true};
-export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false}
+export const INITIAL_STATE_COUNTER: IState = { counter: 0 };
+export const INITIAL_STATE_SHOW_COUNTER: IShowCounter = { showCounter: true};
+
+export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false};
 
 export const COUNTER: string = "COUNTER";
 export const AUTHENTICATION: string = "AUTHENTICATION";
