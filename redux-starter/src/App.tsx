7c99052fa30e4af2c03b1d255f41fefa34df7c8b
Nick <nsagias@gmail.com>
2022-02-02 01:30:22 -0500

    import Header and Auth

--- redux-starter/src/App.tsx
+++ redux-starter/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">     
+      <Header />
+        <Auth /> 
       <Counter />
     </div>
   );
