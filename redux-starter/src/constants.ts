f2ff48317eb646e50a322d0b613d5823f12f5470
Nick <nsagias@gmail.com>
2022-02-02 17:38:40 -0500

    Add authentication constant and export it

--- redux-starter/src/constants.ts
+++ redux-starter/src/constants.ts
@@ -5,6 +5,7 @@ export const INITIAL_STATE_COUNTER: IState = { counter: 0, showCounter: true};
 export const INITIAL_STATE_AUTHENTICATION: IAuthentication = { isAuthenticated: false}
 
 export const COUNTER: string = "COUNTER";
+export const AUTHENTICATION: string = "AUTHENTICATION";
 export const INCREMENT: string = "INCREMENT";
 export const INCREASE: string = "INCREASE";
 export const DECREMENT: string = "DECREMENT";
