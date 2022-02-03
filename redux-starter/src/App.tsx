360c05dff01483240ecd0cdbcd501f87bdb7d680
Nick <nsagias@gmail.com>
2022-02-02 22:26:55 -0500

    import useSelector, useDisptch, RootState, AppDispatch,  UserProfile, and use useDispatch

--- redux-starter/src/App.tsx
+++ redux-starter/src/App.tsx
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from './store';
 
 import './App.css';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  useSelector((state: RootState) => state.)
+
   return (
     <Fragment>
       <Header />
