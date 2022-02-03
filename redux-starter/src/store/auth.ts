b24529e041826d9bd9b342ff4a12b91bcc737b5e
Nick <nsagias@gmail.com>
2022-02-03 00:01:39 -0500

    create authstore

---
+++ redux-starter/src/store/auth.ts
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { IAuthentication } from "../authentication.model";
+import { AUTHENTICATION, INITIAL_STATE_AUTHENTICATION } from "../constants";
+
+
+const authenticationSlice = createSlice({
+  name: AUTHENTICATION,
+  initialState: INITIAL_STATE_AUTHENTICATION,
+  reducers: {
+    login(state: IAuthentication): void {
+      state.isAuthenticated = true;
+    },
+    logout(state: IAuthentication): void {
+      state.isAuthenticated = false;
+    }
+  }
+});
+
+export default authenticationSlice;
