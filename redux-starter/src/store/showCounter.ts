018ca2bf10cb25ddaddb7bddb900049567d29e2f
Nick <nsagias@gmail.com>
2022-02-03 00:04:06 -0500

    refactor showCounter

---
+++ redux-starter/src/store/showCounter.ts
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { SHOWCOUNTER, INITIAL_STATE_SHOW_COUNTER } from "../constants";
+import { IShowCounter } from "../showCounter.model";
+
+const showCounterSlice = createSlice({
+  name: SHOWCOUNTER,
+  initialState: INITIAL_STATE_SHOW_COUNTER,
+  reducers: {
+    toggleCounter(state: IShowCounter): void {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+export default showCounterSlice;
